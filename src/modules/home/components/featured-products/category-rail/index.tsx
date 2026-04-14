519d4fbe90a1219b84cf357765c965515007ba78
Amir <amirkodi57@gmail.com>
2026-04-13 21:26:04 -0700

    Fixed categories cache issue

--- src/modules/home/components/featured-products/category-rail/index.tsx
+++ src/modules/home/components/featured-products/category-rail/index.tsx
@@ -23,9 +23,9 @@ export default async function CategoryRail({
   if (!products?.length) return null
 
   return (
-    <div className="content-container py-12 small:py-24">
+    <div className="content-container pt-4 pb-0">
       <h2 className="text-4xl font-bold mb-8">{category.name}</h2>
-      <ul className="grid grid-cols-2 small:grid-cols-4 gap-x-6 gap-y-24 small:gap-y-36">
+      <ul className="grid grid-cols-2 small:grid-cols-4 gap-x-6 gap-y-8">
         {products.map((product) => (
           <li key={product.id}>
             <ProductPreview product={product} region={region} isFeatured />
